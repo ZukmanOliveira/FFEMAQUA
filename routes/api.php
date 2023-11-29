d85a4aef63926f0e9b42a3d7487ed014e01c8302
ZukmanOliveira <ronildowod@gmail.com>
2023-11-29 19:33:26 -0300

    first-commit

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,34 @@
 <?php
-
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\toolsController;
-/*
+
+
+Route::middleware('auth:api')->get('/user', function( Request $request){
+    return $request->user();
+});
+
+
 Route::prefix('auth')->middleware('jwt.auth')->group( function(){
-    Route::post('tools/', [toolsController::class, 'store']);
+    Route::get('/tools', [toolsController::class, 'index']);  
     Route::delete('/tools/{id}', [toolsController::class, 'refresh']);
     
 });
-*/
+
 
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('login', [AuthController::class,'login']);
     Route::post('logout', [AuthController::class,'logout']);
     Route::post('refresh', [AuthController::class,'refresh']);
     Route::post('me', [AuthController::class,'me']);
+    Route::get('tools', [toolsController::class, 'index']);
 
 });
-
-Route::get('/tools', [toolsController::class, 'index']);
-Route::post('/tools', [toolsController::class, 'store']);
+Route::post('login', [AuthController::class,'login']);
+//Route::get('/tools', [toolsController::class, 'index']);
+//Route::post('/tools', [toolsController::class, 'store']);
 Route::delete('/tools/{id}', [toolsController::class, 'destroy']);
 Route::post('/register',[AuthController::class,'register']);
 Route::get('/tools/{id}', [toolsController::class, 'show']);
